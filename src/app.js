f0749f68ded46c2acb841acb35631f637da9d854
danilo.albudane <daniloassis_vg@hotmail.com>
2020-08-08 17:54:03 -0300

    Rendering condicional

---
+++ src/app.js
@@ -0,0 +1,66 @@
+console.log('App is running')
+
+//JSX - JavaScript XML
+
+var app = {
+  title : 'Indecision App',
+  subtitle : "Put your life in the hand of a computer",
+  options : ['One', 'Two']
+};
+
+
+var template = (
+<div>
+  <h1>{app.title}</h1> 
+  {app.subtitle && <p>{app.subtitle}</p>}
+  <p>{app.options.length > 0 ? 'Here are your options' : 'no options' }</p>
+  <ol>
+    <li>Item one</li>
+    <li>Item two</li>
+  </ol>
+</div>
+);
+
+var user = {
+  Name: 'Andrew',
+  Age: '20',
+  Location: 'RiodeJaneiro'
+}
+
+function getLocation(location){
+  if (location) {
+    return <p>location: {location}</p>;
+  }
+}
+var templateTwo = (
+  <div>
+    <h1>{user.Name ? user.Name : 'Anonymous'}</h1> 
+    {(user.Age && user.Age >= 18) && <p>Age: {user.Age}</p>}
+    {getLocation(user.Location)}
+    
+  </div>
+  );
+
+
+// function getLocation(location){
+//   if (location) {
+//     return location;
+//   }else{
+//     return 'unknown';
+//   }
+// }
+// var userName = 'Andrew';
+// var Age = '29';
+// var location = 'RiodeJaneiro';
+// var templateThree = (
+//   <div>
+//     <h1>{user.Name}</h1> 
+//     <p>{user.Age}</p>
+//     <p>{getLocation(user.Location)}</p>
+    
+//   </div>
+//   );
+
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(templateTwo, appRoot);
